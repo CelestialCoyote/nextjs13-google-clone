06888709baa595991fc04718a7e97123f3eed9f3
Paul Stearns <celestialcoyote@gmail.com>
2023-06-03 15:29:35 -0700

    added pagination of results

--- src/app/search/web/page.js
+++ src/app/search/web/page.js
@@ -3,8 +3,9 @@ import WebSearchResults from "@/components/WebSearchResults";
 
 
 export default async function WebSearchPage({ searchParams }) {
+	const startIndex = searchParams.start || "1";
 	const response = await fetch(
-		`https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.GOOGLE_CONTEXT_KEY}&q=${searchParams.searchTerm}`
+		`https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.GOOGLE_CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image&start=${startIndex}`
 	);
 
 	if (!response.ok) {
--- src/components/ImageSearchResults.js
+++ src/components/ImageSearchResults.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import PaginationButtons from './PaginationButtons';
 
 
 export default function ImageSearchResults({ results }) {
@@ -25,6 +26,9 @@ export default function ImageSearchResults({ results }) {
 						</div>
 					</div>
 				))}
+			</div>
+			<div className='ml-16'>
+				<PaginationButtons />
 			</div>
 		</div>
 	)
